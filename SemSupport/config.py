c1d2ad85b63e4e5c1ba4218af5a6438f22b8b236
zhuoxiaojian <1933860854@qq.com>
2018-08-11 11:09:50 +0800

    修改配置

--- SemSupport/config.py
+++ SemSupport/config.py
@@ -10,12 +10,12 @@ from SemSupport.settings import TIME_ZONE
 from celery.schedules import crontab
 # djcelery.setup_loader()
 #本地
-BROKER_URL = 'redis://:123456@127.0.0.1:6379/1'
-CELERY_RESULT_BAKEND = 'redis://:123456@127.0.0.1:6379/2'
+# BROKER_URL = 'redis://:123456@127.0.0.1:6379/1'
+# CELERY_RESULT_BAKEND = 'redis://:123456@127.0.0.1:6379/2'
 
 #测试服
-# BROKER_URL = 'redis://:123456@smsredis:6379/1'
-# CELERY_RESULT_BAKEND = 'redis://:123456@smsredis:6379/2'
+BROKER_URL = 'redis://:123456@smsredis:6379/1'
+CELERY_RESULT_BAKEND = 'redis://:123456@smsredis:6379/2'
 
 # celery内容等消息的格式设置
 CELERY_ACCEPT_CONTENT = ['application/json']
