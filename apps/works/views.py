418550e0e5ec07feda8242a7e0ce057e80cf6fd7
zhuoxiaojian <1933860854@qq.com>
2019-09-19 21:01:25 +0800

    添加 一键清空备份任务 功能

--- apps/works/views.py
+++ apps/works/views.py
@@ -5,7 +5,8 @@ from django.shortcuts import render
 from django.views.generic import View
 from customers.tasks import divide_the_work
 from django.http import JsonResponse
-
+from works.models import BackUpWork
+from utils.DateFormatUtil import get_today
 
 class OneKeyInitWork(View):
     def get(self, request):
@@ -14,3 +15,13 @@ class OneKeyInitWork(View):
             return JsonResponse({"msg": "请登录后重试！！"})
         divide_the_work()
         return JsonResponse({"msg": "任务分配成功！！"})
+
+
+class OneKeyClearWork(View):
+    def get(self, request):
+        user = request.user
+        if user == AnonymousUser():
+            return JsonResponse({"msg": "请登录后重试！！"})
+        today_str = str(get_today())
+        BackUpWork.objects.filter(create_time=today_str).delete()
+        return JsonResponse({"msg": "当天任务备份清空成功！！"})
